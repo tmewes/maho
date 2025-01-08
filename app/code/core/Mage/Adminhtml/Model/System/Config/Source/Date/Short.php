2050461b440d6c6b668a56a928897cfee862a8dc
Hans Mackowiak <h.mackowiak@b3-it.de>
2025-01-08 22:46:14 +0100

    Deprecate Date_Short and removed strftime (#4462)
    Deprecate Class because the values are strftime format strings
    
    removed strftime and replace with date calls

--- app/code/core/Mage/Adminhtml/Model/System/Config/Source/Date/Short.php
+++ app/code/core/Mage/Adminhtml/Model/System/Config/Source/Date/Short.php
@@ -12,6 +12,7 @@
 /**
  * @category   Mage
  * @package    Mage_Adminhtml
+ * @deprecated
  */
 class Mage_Adminhtml_Model_System_Config_Source_Date_Short
 {
@@ -19,10 +20,10 @@ class Mage_Adminhtml_Model_System_Config_Source_Date_Short
     {
         $arr = [];
         $arr[] = ['label' => '', 'value' => ''];
-        $arr[] = ['label' => strftime('MM/DD/YY (%m/%d/%y)'), 'value' => '%m/%d/%y'];
-        $arr[] = ['label' => strftime('MM/DD/YYYY (%m/%d/%Y)'), 'value' => '%m/%d/%Y'];
-        $arr[] = ['label' => strftime('DD/MM/YY (%d/%m/%y)'), 'value' => '%d/%m/%y'];
-        $arr[] = ['label' => strftime('DD/MM/YYYY (%d/%m/%Y)'), 'value' => '%d/%m/%Y'];
+        $arr[] = ['label' => sprintf('MM/DD/YY (%s)', date('m/d/y')), 'value' => '%m/%d/%y'];
+        $arr[] = ['label' => sprintf('MM/DD/YYYY (%s)', date('m/d/Y')), 'value' => '%m/%d/%Y'];
+        $arr[] = ['label' => sprintf('DD/MM/YY (%s)', date('d/m/y')), 'value' => '%d/%m/%y'];
+        $arr[] = ['label' => sprintf('DD/MM/YYYY (%s)', date('d/m/Y')), 'value' => '%d/%m/%Y'];
         return $arr;
     }
 }
